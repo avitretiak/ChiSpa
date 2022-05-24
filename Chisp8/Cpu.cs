58b3ec5a43ede7e0819ab0b389289c78ef48f917
Avi Tretiak <avitretiak@gmail.com>
2022-05-24 01:09:13 -0300

    Cleanup CPU.cs, add Tick logic

--- Chisp8/Cpu.cs
+++ Chisp8/Cpu.cs
@@ -4,21 +4,27 @@ using System.Text;
 
 namespace Chisp8
 {
-    class Cpu
+    class CPU
     {
-        private readonly byte[] RAM = new byte[0x1000];
-        private readonly byte[] V = new byte[16];
-        private ushort I = 0, PC = 0x200;
-        private ushort[] Stack = new ushort[16];
+        private readonly byte[] memory = new byte[0x1000], registers = new byte[16]; // 4kb of ram and 16 8-bit registers
+        private ushort index = 0, programCounter = 0x200;
+        private ushort[] stack = new ushort[16]; // stack for 16-bit addresses
 
-        private byte _dt = 0;
-        private byte _st = 0;
+        private byte delayTimer = 0, soundTimer = 0;
 
         private Dictionary<byte, Action<OpCode>> OpCodes;
         public void Tick()
         {
-
+            ushort data = (ushort)(memory[programCounter++] << 8 | memory[programCounter++]); // load next opCode
+            OpCode opCode = new OpCode()
+            {
+                Data = data,
+                NNN = (ushort)(data & 0x0FFF),
+                NN = (byte)(data & 0x00F),
+                N = (byte)(data & 0x000F),
+                X = (byte)((data & 0x0F00) >> 8),
+                Y = (byte)((data & 0x00F0) >> 4),
+            };
         }
-
     }
 }
